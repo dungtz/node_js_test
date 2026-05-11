c9ba9c4117c66081aa184a282d7c009ac566a32d
Dung Tran <tumudu@gmail.com>
2026-05-11 10:22:19 +0700

    test

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Middleware để đọc dữ liệu JSON
 app.use(express.json());
 
-// Route cơ bản
 app.get('/', (req, res) => {
     res.send('Chào mừng bạn đến với dự án Node.js đầu tiên!');
 });
 
-// Một API giả lập trả về danh sách sản phẩm
 app.get('/api/products', (req, res) => {
     const products = [
         { id: 1, name: 'Laptop', price: 1000 },
